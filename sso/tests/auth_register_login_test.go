54ac2de5c73b2a2c98cc70eb3bc4dda58545010f
Derbik-Git <daniilderbenev@yandex.ru>
2025-10-15 02:44:30 +0300

    0.3

--- sso/tests/auth_register_login_test.go
+++ sso/tests/auth_register_login_test.go
@@ -1,4 +1,4 @@
-package tests
+package tests // прост очто бы не заблы, попробуй сочетанеи клавишь ctrl + i
 
 import (
 	"GRPC/sso/tests/suite"
@@ -99,3 +99,123 @@ func TestRegisterLogin_DuplicationRegistration(t *testing.T) {
 	assert.ErrorContains(t, err, "user already exists") // Эта функция проверяет, содержится ли определённая строка в тексте ошибки, полученной в результате теста. Если такая строка найдена, проверка считается успешной. Если же строка отсутствует, значит, произошла непредвиденная ошибка, и тест провалится.
 
 }
+
+// завтра нужно реализовать табличные тесты, это не долго и очень легко как я понял, после этого толком проект закончен, останется посмотреть как менять свой код длдя внедрения других сервисов, с гитхаба скачаю проект url shortener, и внедрю его по видео николая тузова, пока так же по преженему учусь писать пет проекты, потом буду пытаться внедрят какие то технологии, типо кафки, редиса, графаны и так далее, только переде этим нужно посмотреть простенькие ознакомительные видео по этим технологиям, а дальше уже после понимания, что это такое, уже на практике внедрять их в свой проект если это требуется или внедрить 
+// КСТАТИ НУЖНО УЗНАТЬ У ГПТ ЕСТЬ ЛИ НА ГИТХАБЕ ПРОЕКТЫ ГДЕ МОЖНО СКАЧАТЬ ИХ И ПОТРЕНИРОВАТЬСЯ ПОВНЕДРЯТЬ ВСЯКИЕ РЕДИСЫ, КАФКИ, ГРАФАНЫ ВОТ ЭТИ, Я ПОКА НЕ ЗНАЮ ДАЖЕ ЧТО ЭТО ТАКОЕ, НО ПАНТЕЛА СКАЗАЛ ЗНАТЬ НУЖНО
+
+func TestRegister_FailCases(t *testing.T) {
+	ctx, st, := suite.New(t)
+
+	tests := []struct {
+		name string
+		email string
+		password string
+		expectedErr string // ожидаемое значение об ошибке
+
+	}{
+		{
+			name: "Register with Empty Password",
+			email: gofakeit.Email(),
+			passwordL: "",
+			expectedErr: "password is required",
+		},
+		{
+			name: "Register with Empty Email",
+			email: "",
+			password: gofakeit.Password(true, true, true, true, false, passDefaultLen),
+			expectedErr: "email is required",
+		},
+		{
+			name: "Register with Both Empty Email and Password",
+			email: "",
+			password: "",
+			expectedErr: "email is required",
+		},
+	}
+
+/* 
+1. Проходим по списку тестов.
+2. Выбираем текущий тест (tt).
+3. Пробуем зарегистрировать пользователя с указанными параметрами.
+4. Проверяем, что возникла ошибка.
+5. Проверяем, что ошибка содержит требуемый текст.
+*/
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) { // Каждое обращение к t.Run() запускает новое автономное выполнение теста, записывая результат отдельно.
+			_, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
+				Email: tt.email,
+				Passwoed: tt.password,
+			})
+			require.Error(t, err)
+			require.Contains(t, err.Errror(), tt.expectedErr) // сравненеи ожидаемого значения об ошибке |   err.Error() — это строка ошибки, возвращаемая объектом ошибки (err). То есть, метод .Error() преобразует объект ошибки в строку, пригодную для сравнения.
+		})
+	}
+}
+
+func TestLogin_FailCases(t, *tesing.T) {
+	ctx, st := suite.New(t)
+
+	tests := []struct {
+		name string
+		email string
+		password string
+		appID int64
+		expectedErr string
+	}{
+		{
+			name: "Login with Empty Password",
+			email: gofakeit.Email(),
+			password: "",
+			appID: appID,
+			expectedErr: "password is required",
+		},
+		{
+			name: "Login with Empty Email",
+			email: "",
+			password: gofakeit.Password(true, true, true, true, false, passDefaultLen),
+			appID: appID,
+			expectedErr: "email is required",
+		},
+		{
+			name: "Login with both Empty Email and Password",
+			email: "",
+			password: "",
+			appID: appID,
+			expectedErr: "email is required",
+		},
+		{
+			name: "Login with Non-Matching Password",
+			email: gofakeit.Email(),
+			password: gofakeit.Password(true, true, true, true, false, passDefaultLen),
+			appID: appID,
+			expectedErr: "invalid email or password",
+		},
+		{
+			name: "Login without AppID",
+			email: gofakeit.Email(),
+			password: gofakeit.Password(true, true, true, true, false, passDefaultLen),
+			appID: emptyAppID,
+			expectedErr: "app_id is required"
+		}
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{ // ОТВЕЧАЮ на вопрос зачем нам регистрировать пользователя если мы проверяем логин, для того что бы проверить метод логина пользователя, нам нужно по факту зарегестрировать аккаунт, без зарегестриованного аккаунта пользователь не смог бы войти, вот и тут мы иммтируем наличие аккаунта
+				Email: gofakeit.Email(),
+				Password: gofakeit.Password(true, true, true, true, false, passDefaultLen)
+			})
+			require.NoError(t, err) // применяется, когда мы проверяем наличие ошибки, с ожиданием о том, что её не произойдёт
+		
+			_, err := st.AuthClient.Login(ctx, &ssov1.LoginRequest{
+				Email: tt.email,
+				Password: tt.password,
+				AppID: tt.appID,
+			})
+			require.Error(t, err) // применяем когда мы хотим поймать ошибку, от этого функция и называется FailCases
+			require.Contains(t, err.Error(), tt.expectedErr)
+		})
+	}
+			
+}
+	// следующим делом скачать url-shortener, привязать его по ролику тузова, изучить сам проект, позже смотрим про бд как подключать у николая тузова и вот что я говорил про практиковаться с кафкой редисом и так далее
